9cf8c82597a22c47e9587e282d90d9e24faed1bc
VasylTyshchuk <vasya.tys@gmail.com>
2022-07-17 17:03:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# dmenu
+
+[dmenu](https://tools.suckless.org/dmenu/) is an efficient dynamic menu for X.
+
+## Patches
+
+- [fuzzymatch](https://tools.suckless.org/dmenu/patches/fuzzymatch/)
+- [mouse support](https://tools.suckless.org/dmenu/patches/mouse-support/)
+- emoji support (libxft-bgra package must be installed. By default JoyPixels and JetBrainsMono Nerd font are used for emoji, you can specify your own fonts in `config.def.h`)
+- `ctrl + v`, `ctrl + shift + v` support
---
+++ config.def.h
@@ -0,0 +1,29 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+/* Pywal */
+/* #include "/home/basilios/.cache/wal/colors-wal-dmenu.h" */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+    "JetBrainsMono Nerd Font:size=12",
+    /* Emoji font */
+    "JoyPixels:pixelsize=12:antialias=true:autohint=true"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#eff5f9", "#17212b" },
+	[SchemeSel] = { "#17212b", "#eff5f0" },
+	[SchemeOut] = { "#eff5f9", "#5c7aea" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
