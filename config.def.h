b7067112ee1fdeddda1308a66b8e187f01d2c3d1
VasylTyshchuk <vasya.tys@gmail.com>
2022-07-17 17:56:30 +0300

    Add backup font

--- config.def.h
+++ config.def.h
@@ -10,7 +10,9 @@ static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't us
 static const char *fonts[] = {
     "JetBrainsMono Nerd Font:size=12",
     /* Emoji font */
-    "JoyPixels:pixelsize=12:antialias=true:autohint=true"
+    "JoyPixels:pixelsize=12:antialias=true:autohint=true",
+    /* Backup font */
+    "monospace:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
